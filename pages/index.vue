db671c673ac743e8a65ae9d9cff9d637521995a6
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2021-08-08 15:05:31 +0300

    add sass-loader

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,28 @@
 <template>
   <div class="container">
-    <div class="row">
-      <div class="col-4">
-        app 1
+    <header>
+      <div class="header-panel">
+        <a href="#" class="header-panel_logo">logo</a>
       </div>
-      <div class="col-8">
-        app 2
+      <div class="header-top">
+        <h1 class="header-top_t">
+          Lorem ipsum dolor sit amet.
+        </h1>
+        <p class="header-top_tx">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A adipisci animi cupiditate
+          delectus distinctio doloribus est iste iure, minus molestias mollitia nemo quia quos tempore voluptatem?
+          Aliquid cum eaque eligendi, error esse fugiat ipsam libero minima molestias perspiciatis, placeat sit.
+        </p>
       </div>
-    </div>
+    </header>
   </div>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style lang="sass">
+  header
+    background-color: #eeeeee
+</style>

206ed71e3f17aca6a37675596b80403ca5b28b69
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2021-08-08 14:14:05 +0300

    add bootstrap-grid

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <Tutorial />
+  <div class="container">
+    <div class="row">
+      <div class="col-4">
+        app 1
+      </div>
+      <div class="col-8">
+        app 2
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>

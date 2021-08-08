cbc5c60b799156431cff1684318aeaa54b45e256
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2021-08-08 15:45:41 +0300

    add header on main page

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~assets/css/bootstrap-grid.css'
+    '~assets/css/bootstrap-grid.css',
+    '~assets/css/bootstrap-reboot.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="container">
-    <header>
-      <div class="header-panel">
-        <a href="#" class="header-panel_logo">logo</a>
-      </div>
-      <div class="header-top">
-        <h1 class="header-top_t">
-          Lorem ipsum dolor sit amet.
-        </h1>
-        <p class="header-top_tx">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A adipisci animi cupiditate
-          delectus distinctio doloribus est iste iure, minus molestias mollitia nemo quia quos tempore voluptatem?
-          Aliquid cum eaque eligendi, error esse fugiat ipsam libero minima molestias perspiciatis, placeat sit.
-        </p>
-      </div>
-    </header>
+    <div class="main-page">
+      <header>
+        <div class="header-panel">
+          <a href="#" class="header-panel_logo">KATTAgroceries</a>
+        </div>
+        <div class="header-top">
+          <h1 class="header-top_t">
+            <span class="header-top_subt">Your shopping, picked personally!</span>
+            KATTA good living.
+          </h1>
+          <p class="header-top_tx">
+            You'll find food news, recipes, lifestyle hints, hacks and tips, family activities, community and more.
+            Katta stores house all sorts of services to make shopping with us, fast and friendly.
+          </p>
+        </div>
+      </header>
+    </div>
   </div>
 </template>
 
@@ -23,6 +25,39 @@ export default {}
 </script>
 
 <style lang="sass">
+.main-page
+  border: 1px solid #ededed
+  font-family: Arial, sans-serif
   header
-    background-color: #eeeeee
+    position: relative
+    background: url('~assets/images/bg-header.jpg') no-repeat 50% 60%
+    background-size: cover
+    color: #ffffff
+    .header-panel
+      background-color: #ffffff
+      &_logo
+        display: block
+        padding: 10px 20px
+        color: #444
+        text-decoration: none
+        &:hover
+          opacity: .9
+    .header-top
+      padding: 60px
+      &_t
+        font-size: 60px
+        font-weight: 400
+        letter-spacing: -1px
+        line-height: 1em
+        margin-bottom: 0
+      &_subt
+        display: block
+        margin: 0
+        font-weight: 400
+        font-size: 30px
+      &_tx
+        font-size: 17px
+        max-width: 600px
+        line-height: 23px
+        margin-top: 35px
 </style>
